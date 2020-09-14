7106d0e4b6329c999055cdee703c012465f3b446
mihaialexandru <mihai.anghel970316@gmail.com>
2020-09-14 13:39:02 +0300

    Rename component files

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import VitalSignsHeader from './components/header-component/header-component.component'
-import VitalSignsFooter from './components/footer-component/footer-component.component'
-import VitalSignsCard from './components/card-component/vital-sign-component.component'
+import VitalSignsHeader from './components/header-component/header.component.component'
+import VitalSignsFooter from './components/footer-component/footer.component.component'
+import VitalSignsCard from './components/card-component/vital.sign.component.component'
 
 export const VitalSignsComponent = () => {
   return (
