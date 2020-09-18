b6310123dc8e9ea830c81c84b979b1212716aee1
mihaialexandru <mihai.anghel970316@gmail.com>
2020-09-18 13:37:26 +0300

    Fix header and footer width Change em unit to px in whole project

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import * as React from 'react'
 import VitalSignsHeader from './components/header-component/header.component.component'
 import VitalSignsFooter from './components/footer-component/footer.component.component'
 import VitalSignItemView from './components/itemview-component/vital.sign.component'
+import style from './styles.module.css'
 
 export const VitalSignsComponent = () => {
   return (
-    <div>
+    <div className={style.vitalSign}>
       <VitalSignsHeader />
       <VitalSignItemView />
       <VitalSignsFooter />
