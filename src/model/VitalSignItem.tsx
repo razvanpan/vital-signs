468d8e9c21b52a98a346297e3445c0af52bd6953
mihaialexandru <mihai.anghel970316@gmail.com>
2020-09-22 10:24:16 +0300

    Refactoring the constructor in VitalSignItem.tsx

--- src/model/VitalSignItem.tsx
+++ src/model/VitalSignItem.tsx
@@ -4,5 +4,10 @@ export default class VitalSignItem {
     public value: string,
     public unitMeasure: string,
     public time: string
-  ) {}
+  ) {
+    this.vitalSign = vitalSign
+    this.value = value
+    this.unitMeasure = unitMeasure
+    this.time = time
+  }
 }
